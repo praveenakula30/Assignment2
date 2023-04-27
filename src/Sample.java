bf567b2c4ab2d47d72ea20b8704e2e8962c9c262
praveenakula30 <praveenaka30@gmail.com>
2023-04-27 12:24:50 +0530

    first commit

---
+++ src/Sample.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+public class Sample{
+public static void main(String[] args) {
+    int a=100;
+    String b="venu";
+    System.out.println(a);
+    System.out.println(b);
+}
+}
